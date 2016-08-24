9fd4238ed5ce6a4878f2125b8459ad37278a6a9c
mrgarry <harijs@gentum.lv>
2016-08-24 13:57:11 +0300

    Update GrowlAsset.php

--- GrowlAsset.php
+++ GrowlAsset.php
@@ -1,7 +1,6 @@
 <?php
 
 /**
- * @link http://gentum.lv
  * @copyright Copyright (c) 2015 Harijs Puga
  * @license MIT
  */
@@ -10,14 +9,10 @@ namespace gentumsoft\growl;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Harijs Puga <harijs98@gmail.com>
- */
-
 class GrowlAsset extends AssetBundle
 {
 
-    public $sourcePath = '@vendor/gentumsoft/growl/assets';
+    public $sourcePath = '@vendor/omj/yii2-growl/assets';
 
     public $depends = [
         'yii\web\JqueryAsset',
